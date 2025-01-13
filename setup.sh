4475d2d5117e519783a52646d271cdb1157e0813
Noel Miller <noel@noelmiller.dev>
2025-01-13 01:14:20 -0600

    used printf instead of echo and removed pipefail command

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
-set -ouex pipefail
 
 # Install brew packages
+printf "Installing brew packages...\n"
 ./.scripts/brew/install_brew_packages
 
 # Install github copilot
+printf "Installing github copilot...\n"
 ./.scripts/github_copilot/install_github_copilot
 
 # Symlink dotfiles
-echo "Symlinking dotfiles..."
-echo "Removing bashrc..."
+printf "Symlinking dotfiles...\n"
+printf "Removing bashrc...\n"
 rm -f ~/.bashrc
 stow .
 
 # Configure ssh-agent systemd service
-echo "Configuring ssh-agent systemd service..."
+printf "Configuring ssh-agent systemd service...\n"
 ./.scripts/systemd/configure_ssh_agent
+
+# Configure fzf
+printf "Configuring fzf...\n"
+$(brew --prefix)/opt/fzf/install
