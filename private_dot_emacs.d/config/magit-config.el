88785f5138d5c84e66c8af9999ab7c84d6ff8b65
Noel Miller <noel@noelmiller.dev>
2026-01-09 14:44:32 -0600

    updated magit to have a dedicated window

--- private_dot_emacs.d/config/magit-config.el
+++ private_dot_emacs.d/config/magit-config.el
@@ -9,7 +9,33 @@
   :straight t)
 
 (use-package magit
-  :straight t)
+  :straight t
+  :custom
+  ;; 1. Open the status buffer in a way that doesn't take over the frame
+  (magit-display-buffer-function #'magit-display-buffer-traditional)
+  
+  ;; 2. Ensure quitting deletes the window rather than "restoring" to dashboard
+  (magit-bury-buffer-function #'quit-window)
+  
+  :config
+  (setq magit-display-buffer-alist
+        '(("(buffer-name . \"\\*magit: .*\")"
+           (display-buffer-reuse-window display-buffer-at-bottom)
+           (window-height . 0.4)
+           (inhibit-same-window . t))))
+  (setq magit-status-sections-hook
+        '(magit-insert-status-headers
+          magit-insert-merge-log
+          magit-insert-rebase-sequence
+          magit-insert-am-sequence
+          magit-insert-sequencer-sequence
+          magit-insert-staged-changes
+          magit-insert-unstaged-changes
+          magit-insert-untracked-files
+          magit-insert-unpushed-to-pushremote
+          magit-insert-unpushed-to-upstream-or-recent
+          magit-insert-unpulled-from-pushremote
+          magit-insert-unpulled-from-upstream)))
 
 (provide 'magit-config)
 
